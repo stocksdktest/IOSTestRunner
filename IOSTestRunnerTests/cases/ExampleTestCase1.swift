773a8712e0ee6d929b60a35068f6e431de25a79b
lxs <lxs137@hotmail.com>
2020-04-19 01:20:45 +0800

    support TestcaseConfig.continueWhenFailed

--- IOSTestRunnerTests/cases/ExampleTestCase1.swift
+++ IOSTestRunnerTests/cases/ExampleTestCase1.swift
@@ -16,23 +16,25 @@ class ExampleTestCase1: BaseTestCase {
         return StockTestCaseName.QUOTE_REQUEST_EXAMPLE
     }
     
-    func testExample1() {
+    func testExample1() throws {
         let param = self.testCaseRoundConfig.getParam()
         let mRequest = MQuoteRequest()
         mRequest.code = param["QUOTE_NUMBERS"].stringValue
         mRequest.stockFields = ["-1"]
         mRequest.addValueFields = ["ddz", "largeSellCount", "mediumDiffer"]
-        let resp = self.makeSyncRequest(request: mRequest)
+        let resp = try self.makeSyncRequest(request: mRequest)
         let quoteResponse = resp as! MQuoteResponse
-        XCTAssertNotNil(quoteResponse.stockItems)
+        if (quoteResponse.stockItems == nil) {
+            throw BaseTestError.assertFailedError(message: "NQuoteResponse stockItems is nil")
+        }
         for item in quoteResponse.stockItems! {
-            print("\(item.id),\(item.name),\(item.lastPrice),\(item.openPrice),\(item.highPrice),\(item.lowPrice),\(item.preClosePrice),\(item.averagePrice),\(item.volume),\(item.amount)")
-            print("最新价：\(item.lastPrice),最高价：\(item.highPrice)")
-//            var resultJSON: JSON = [
-//                "lastPrice": item.lastPrice!,
-//                "highPrice": item.highPrice!
-//            ]
-            onTestResult(param: param, result: JSON())
+            if (item.lastPrice == nil) {
+                throw BaseTestError.assertFailedError(message: "StockItem lastPrice is nil")
+            }
+            var resultJSON: JSON = [
+                "lastPrice": item.lastPrice
+            ]
+            onTestResult(param: param, result: resultJSON)
         }
 //        Utils.log(tag: "ExampleTestCase1", str: "response: \(resp)")
     }
