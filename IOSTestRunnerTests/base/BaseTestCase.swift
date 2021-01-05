dcabc9353a654100c50c91b2d425bce13482c1a8
git208 <wang_hlong@cvicse.com>
2021-01-05 14:06:17 +0800

    0

--- IOSTestRunnerTests/base/BaseTestCase.swift
+++ IOSTestRunnerTests/base/BaseTestCase.swift
@@ -71,7 +71,7 @@ class BaseTestCase : ParametrizedTestCase {
     internal func makeSyncRequest(request: MRequest) throws -> MResponse {
         var reqResponse: MResponse? = nil
         let runLoop = CFRunLoopGetCurrent()
-        print("当前SDK版本为：\(MApi.version())")
+        print("当前SDK版本为：\(MApi.version() ?? "未获取到SDK版本信息")")
         MApi.send(request, completionHandler: { resp in
             reqResponse = resp
             CFRunLoopStop(runLoop)
